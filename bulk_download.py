a014bd4c63e9cfb7b69dee48cc3b6cafcd5a68b6
Dextricity <118403937+iAmDextricity@users.noreply.github.com>
2023-12-11 16:01:11 -0500

    Update bulk_download.py

--- bulk_download.py
+++ bulk_download.py
@@ -7,8 +7,11 @@ import tkinter as tk
 from tkinter import ttk
 from concurrent.futures import ThreadPoolExecutor
 from ttkthemes import ThemedStyle
+import zipfile
+import rarfile
+import chard
 
-current_version = "2.84"
+current_version = "3.0a"
 url = "https://raw.githubusercontent.com/iAmDextricity/Bulk-Downloader/main/version"
 
 # Function to create a requests session
@@ -69,6 +72,8 @@ def download_file(url, destination_folder=DEFAULT_DOWNLOAD_DIR):
         os.rename(downloading_file_name, file_name)
         print(f"File downloaded successfully: {file_name}")
 
+        return file_name  # Return the downloaded file path
+
     except requests.exceptions.RequestException as e:
         print(f"Failed to download file. Error: {e}")
         if os.path.exists(downloading_file_name):
@@ -79,6 +84,39 @@ def download_file(url, destination_folder=DEFAULT_DOWNLOAD_DIR):
         # Close the session to release resources
         if 'session' in locals():
             session.close()
+
+def extract_file(file_path):
+    try:
+        file_dir = os.path.dirname(file_path)
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(file_dir)
+        print(f"File extracted successfully: {file_path}")
+    except zipfile.BadZipFile:
+        try:
+            with rarfile.RarFile(file_path, 'r') as rar_ref:
+                rar_ref.extractall(file_dir)
+            print(f"File extracted successfully: {file_path}")
+        except rarfile.Error as rar_error:
+            print(f"Failed to extract file. Error: {rar_error}")
+    except Exception as e:
+        print(f"An unexpected error occurred while extracting: {e}")
+
+def convert_to_chd(file_path):
+    try:
+        # Check if the file is an ISO or BIN/CUE
+        _, file_extension = os.path.splitext(file_path)
+        file_extension = file_extension.lower()
+
+        if file_extension in ['.iso', '.bin']:
+            chd_file_path = file_path + '.chd'
+            chd.create(file_path, chd_file_path)
+            print(f"File converted to CHD successfully: {chd_file_path}")
+            os.remove(file_path)  # Delete the source file
+        else:
+            print(f"File format not supported for CHD conversion: {file_path}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred while converting to CHD: {e}")
 
 def get_safe_filename(url):
     return os.path.basename(urlparse(url).path).replace("%20", " ")
@@ -95,6 +133,8 @@ def on_download_click():
     url = ensure_trailing_slash(url)
     max_workers = int(max_workers_entry.get())
     download_dir = download_dir_entry.get()
+    extract_files = extract_var.get()
+    convert_to_chd_files = chd_var.get()
 
     try:
         page = requests.get(url)
@@ -111,8 +151,16 @@ def on_download_click():
                     futures.append(executor.submit(download_file, file_url, download_dir))
 
             for future in tqdm(futures, desc="Downloading files", unit="file", leave=True):
-                future.result()
-            status_label.config(text="All files downloaded")
+                downloaded_file_path = future.result()
+                if downloaded_file_path and extract_files:
+                    extract_file(downloaded_file_path)
+                if downloaded_file_path and convert_to_chd_files:
+                    convert_to_chd(downloaded_file_path)
+
+            if extract_files or convert_to_chd_files:
+                status_label.config(text="All files downloaded and processed")
+            else:
+                status_label.config(text="All files downloaded")
 
     except requests.exceptions.RequestException as e:
         print(f"Failed to fetch content from the URL. Error: {e}")
@@ -120,7 +168,7 @@ def on_download_click():
 # GUI
 root = tk.Tk()
 root.title("Web Scraper Downloader")
-root.geometry("330x320")  # Set the window size
+root.geometry("330x380")  # Set the window size
 
 style = ThemedStyle(root)
 style.set_theme("plastik")  # You can try different themes
@@ -151,11 +199,21 @@ download_dir_entry = ttk.Entry(frame, width=50)
 download_dir_entry.insert(0, DEFAULT_DOWNLOAD_DIR)
 download_dir_entry.grid(column=0, row=5, pady=5, sticky=tk.W)
 
+# Checkbutton for extraction option
+extract_var = tk.BooleanVar()
+extract_checkbutton = ttk.Checkbutton(frame, text="Extract Files", variable=extract_var)
+extract_checkbutton.grid(column=0, row=6, pady=5, sticky=tk.W)
+
+# Checkbutton for CHD conversion option
+chd_var = tk.BooleanVar()
+chd_checkbutton = ttk.Checkbutton(frame, text="Convert  iso or bin/cue to CHD", variable=chd_var)
+chd_checkbutton.grid(column=0, row=7, pady=5, sticky=tk.W)
+
 # Use the rounded style for the download button
 download_button = ttk.Button(frame, text="Download Files", command=on_download_click, style='Rounded.TButton')
-download_button.grid(column=0, row=6, pady=20, sticky=tk.W)
+download_button.grid(column=0, row=8, pady=20, sticky=tk.W)
 
 status_label = ttk.Label(frame, text="")
-status_label.grid(column=0, row=7, sticky=tk.W)
+status_label.grid(column=0, row=9, sticky=tk.W)
 
 root.mainloop()
